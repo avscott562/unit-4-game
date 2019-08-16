36809453faa9d8f5fc1ab989f1853a77480a9ed6
Scottie <avscott@pacbell.net>
2019-08-16 00:33:44 -0700

    add random function to get each crystal value.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,38 @@
 //declare variables
 //count for wins and losses
 //to hold evolving user score
-//to hold computer score
+
+//hold computer score
 var compScore = 0;
-//for each gem to hold current value
+
+//hold current value for each crystal
+var greenScore = 0;
+var pinkScore = 0;
+var yellowScore = 0;
+var rainbowScore = 0;
 
 //functions and actions needed
+
+
 //new game function
-//random function for computer score
 function newGame() {
+    //randomly generate computer score from 19 to 120
     compScore = Math.floor(Math.random() * (120 - 19)) + 19;
     console.log("Hi the goal is " + compScore);
+
+    //randomly generate value of each crystal from 1 to 12
+    //add if statements to ensure each crystal has a different value than the others
+    greenScore = Math.floor(Math.random() * 13);
+    console.log("The value for the green crystal is " + greenScore);
+
+    pinkScore = Math.floor(Math.random() * 13);
+    console.log("The value for the pink crystal is " + pinkScore);
+
+    yellowScore = Math.floor(Math.random() * 13);
+    console.log("The value for the yellow crystal is " + yellowScore);
+
+    rainbowScore = Math.floor(Math.random() * 13);
+    console.log("The value for the rainbow crystal is " + rainbowScore);
 }
 
 newGame();
