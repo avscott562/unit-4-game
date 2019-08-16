1ce9d103c55c7576bbcd3f2eaeb8c357ac583687
Scottie <avscott@pacbell.net>
2019-08-16 00:10:15 -0700

    add psuedocode for js:

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+//declare variables
+//count for wins and losses
+//to hold evolving user score
+//to hold computer score
+//for each gem to hold current value
+
+//functions and actions needed
+//new game function
+//random function for computer score
+//random function for each crystal value
+//on click function for each gem
+//reset function
