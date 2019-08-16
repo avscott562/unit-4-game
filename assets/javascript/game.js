6bbee561ac1f863a77869c66e3a37e1a62233b88
Scottie <avscott@pacbell.net>
2019-08-16 00:25:45 -0700

    add new game function and random for computer guess.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,11 +2,18 @@
 //count for wins and losses
 //to hold evolving user score
 //to hold computer score
+var compScore = 0;
 //for each gem to hold current value
 
 //functions and actions needed
 //new game function
 //random function for computer score
+function newGame() {
+    compScore = Math.floor(Math.random() * (120 - 19)) + 19;
+    console.log("Hi the goal is " + compScore);
+}
+
+newGame();
 //random function for each crystal value
 //on click function for each gem
 //reset function
