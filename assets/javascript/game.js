7c9ecff33ec07c72e973c7eeec0d2f71db9202ee
Scottie <avscott@pacbell.net>
2019-08-16 01:38:29 -0700

    add for loop to generate crystal values and assign to crystals. on click to each crystal and display sum on screen.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,41 +1,60 @@
 //declare variables
+
 //count for wins and losses
+var win = 0;
+var loss = 0;
+
 //to hold evolving user score
+var score = 0;
 
 //hold computer score
 var compScore = 0;
 
-//hold current value for each crystal
-var greenScore = 0;
-var pinkScore = 0;
-var yellowScore = 0;
-var rainbowScore = 0;
-
-//functions and actions needed
+//array to capture current value for each crystal
+var values = [];
 
 
 //new game function
 function newGame() {
+
     //randomly generate computer score from 19 to 120
     compScore = Math.floor(Math.random() * (120 - 19)) + 19;
-    console.log("Hi the goal is " + compScore);
+
+    //display computer score on screen
+    $('#randomNumber').text(compScore);
 
     //randomly generate value of each crystal from 1 to 12
     //add if statements to ensure each crystal has a different value than the others
-    greenScore = Math.floor(Math.random() * 13);
-    console.log("The value for the green crystal is " + greenScore);
+    for (i=0; i<4; i++) {
+        var gemScore = Math.floor(Math.random() * 11) + 1;
+        if (values.includes(gemScore)) {
+            gemScore = Math.floor(Math.random() * 11) + 1;
+        }
+        values.push(gemScore);
+    }
+    console.log(values);
 
-    pinkScore = Math.floor(Math.random() * 13);
-    console.log("The value for the pink crystal is " + pinkScore);
+    //add crystal values to image
+    $('#green').attr('data-crystalValue', values[0]);
+    $('#pink').attr('data-crystalValue', values[1]);
+    $('#yellow').attr('data-crystalValue', values[2]);
+    $('#rainbow').attr('data-crystalValue', values[3]);
 
-    yellowScore = Math.floor(Math.random() * 13);
-    console.log("The value for the yellow crystal is " + yellowScore);
-
-    rainbowScore = Math.floor(Math.random() * 13);
-    console.log("The value for the rainbow crystal is " + rainbowScore);
 }
 
 newGame();
-//random function for each crystal value
-//on click function for each gem
+
+//on click function for each crystal
+$('.crystal').on("click", function() {
+    //grab value of each crystal
+    var crystalValue = parseInt($(this).attr("data-crystalValue"));
+
+    //increment user score by crystal value
+    score += crystalValue;
+    console.log(score);
+
+    //display user score on screen
+    $('#userTotal').text(score);
+});
+
 //reset function
